cd7b4d9f85abbe32281789b27100ece2899e91ff
A.suzuki <suzu.devworks@gmail.com>
2025-11-01 22:08:28 +0900

    Add example database to postgres.

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -13,5 +13,6 @@ dotnet user-secrets --project src/Examples.EntityFrameworkCore.PostgreSQL.Tests 
 dotnet user-secrets --project src/Examples.EntityFrameworkCore.PostgreSQL.Tests list
 
 dotnet user-secrets --project src/Examples.Dapper.PostgreSQL.Tests set ConnectionStrings:ContosoUniversity "Host=${POSTGRES_SERVICE};Database=contoso_university;Username=postgres;Password=$(cat /run/secrets/db_password)"
+dotnet user-secrets --project src/Examples.Dapper.PostgreSQL.Tests set ConnectionStrings:LearnDapper "Host=${POSTGRES_SERVICE};Database=examples;Username=dev;Password=XwkYC\$3MuZrc&T"
 dotnet user-secrets --project src/Examples.Dapper.PostgreSQL.Tests list
 fi
